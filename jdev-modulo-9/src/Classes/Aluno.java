b948ae613a5a85b457befc7770810d9a07956cbe
Igor Santos Oliveira <iigor315@hotmail.com>
2022-06-21 17:35:16 -0300

    Primeira classe e objeto aluno

---
+++ jdev-modulo-9/src/Classes/Aluno.java
@@ -0,0 +1,21 @@
+package Classes;
+
+/**
+ * @author jdev-treinamentos
+ * Os exemplos aqui estão como nas aulas do curso jdev treinamentos.
+ */
+public class Aluno {
+	
+	String nome;
+	int idade;
+	String dataNascimento;
+	String registroGeral;
+	String numeroCpf;
+	String nomeMae;
+	String nomePai;
+	String dataMatricula;
+	String nomeEscola;
+	String serieMatriculado;
+	
+
+}
