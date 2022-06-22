3b42fb42cde7786f459804ab727790fc2abd0da1
Igor Santos Oliveira <iigor315@hotmail.com>
2022-06-22 13:48:25 -0300

    Criandos métodos comuns Getters e Setters

--- jdev-modulo-9/src/Application/Application.java
+++ jdev-modulo-9/src/Application/Application.java
@@ -9,13 +9,33 @@ public class Application {
 //		Aluno1 é uma referência para o objeto Aluno
 //		new Aluno() é uma instância(Criação de Objeto)
 		Aluno aluno1 = new Aluno();
+		aluno1.setNome("Matew");
+		aluno1.setDataNascimento("01/07/2000");
+		aluno1.setIdade(22);
+		aluno1.setRegistroGeral("222-22");
+		aluno1.setNumeroCpf("111-111-111-11");
+		aluno1.setNomeMae("Maria Antonia");
+		aluno1.setNomePai("Sebastiao Texeira");
+		aluno1.setDataMatricula("10/01/2020");
+		aluno1.setNomeEscola("JDEV treinamentos");
+		
+//		recuperamos os dados atraves do aluno1.getNome();
+		System.out.println("Nome do aluno1 = " + aluno1.getNome());
+		System.out.println("Data de nascimento = " + aluno1.getDataNascimento());
+		System.out.println("Idade = " + aluno1.getIdade());
+		System.out.println("Registro geral " + aluno1.getRegistroGeral());
+		System.out.println("Número CPF = " + aluno1.getNumeroCpf());
+		System.out.println("Nome da mãe = " + aluno1.getNomeMae());
+		System.out.println("Nome do pai = " + aluno1.getNomePai());
+		System.out.println("Data da matrícula  = " + aluno1.getDataMatricula());
+		System.out.println("Nome da escola = " + aluno1.getNomeEscola());
+		
 		
 //		Nome do aluno2 "Maria" é um exemplo de atributo do Objeto
 		Aluno aluno2 = new Aluno("Maria");
 		
 		Aluno aluno3 = new Aluno("Joao", 15);
 	
-		System.out.println("Nomse " + aluno1);
 	}
 
 }
--- jdev-modulo-9/src/Classes/Aluno.java
+++ jdev-modulo-9/src/Classes/Aluno.java
@@ -36,4 +36,94 @@ public class Aluno {
 		this.nome = nomePadrao;
 		this.idade = idadePadrao;
 	}
+	
+//	Veremos os metodos SETTERS e GETTERS do objeto
+//	SET é para adicionar ou receber dados para os atributos
+//	GET é para resgatar ou obter o valor dos atributos
+
+	
+//	retorna os dados
+	public String getNome() {
+		return nome;
+	}
+	
+//	recebe os dados
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public int getIdade() {
+		return idade;
+	}
+
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+
+	public String getDataNascimento() {
+		return dataNascimento;
+	}
+
+	public void setDataNascimento(String dataNascimento) {
+		this.dataNascimento = dataNascimento;
+	}
+
+	public String getRegistroGeral() {
+		return registroGeral;
+	}
+
+	public void setRegistroGeral(String registroGeral) {
+		this.registroGeral = registroGeral;
+	}
+
+	public String getNumeroCpf() {
+		return numeroCpf;
+	}
+
+	public void setNumeroCpf(String numeroCpf) {
+		this.numeroCpf = numeroCpf;
+	}
+
+	public String getNomeMae() {
+		return nomeMae;
+	}
+
+	public void setNomeMae(String nomeMae) {
+		this.nomeMae = nomeMae;
+	}
+
+	public String getNomePai() {
+		return nomePai;
+	}
+
+	public void setNomePai(String nomePai) {
+		this.nomePai = nomePai;
+	}
+
+	public String getDataMatricula() {
+		return dataMatricula;
+	}
+
+	public void setDataMatricula(String dataMatricula) {
+		this.dataMatricula = dataMatricula;
+	}
+
+	public String getNomeEscola() {
+		return nomeEscola;
+	}
+
+	public void setNomeEscola(String nomeEscola) {
+		this.nomeEscola = nomeEscola;
+	}
+
+	public String getSerieMatriculado() {
+		return serieMatriculado;
+	}
+
+	public void setSerieMatriculado(String serieMatriculado) {
+		this.serieMatriculado = serieMatriculado;
+	}
+	
+
+	
 }
