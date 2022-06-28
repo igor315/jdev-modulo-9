053b5da299b5e06df57a64f7c529c9ae722ac26e
Igor Santos Oliveira <iigor315@hotmail.com>
2022-06-28 12:10:17 -0300

    Descobrindo se o aluno está em recuperação

--- jdev-modulo-9/src/Classes/Aluno.java
+++ jdev-modulo-9/src/Classes/Aluno.java
@@ -5,11 +5,11 @@ import java.util.List;
 import java.util.Objects;
 
 /**
- * @author jdev-treinamentos
- * Os exemplos aqui estão como nas aulas do curso jdev treinamentos.
+ * @author jdev-treinamentos Os exemplos aqui estão como nas aulas do curso jdev
+ *         treinamentos.
  */
 public class Aluno {
-	
+
 	String nome;
 	int idade;
 	String dataNascimento;
@@ -20,18 +20,19 @@ public class Aluno {
 	String dataMatricula;
 	String nomeEscola;
 	String serieMatriculado;
-	
 
 	private List<Disciplina> disciplinas = new ArrayList<Disciplina>();
+
 	public void setDisciplinas(List<Disciplina> disciplinas) {
 		this.disciplinas = disciplinas;
 	}
+
 	public List<Disciplina> getDisciplinas() {
 		return disciplinas;
 	}
 
 	public Aluno() {
-		
+
 	}
 
 	public Aluno(String nomePadrao) {
@@ -46,7 +47,7 @@ public class Aluno {
 	public String getNome() {
 		return nome;
 	}
-	
+
 //	recebe os dados
 	public void setNome(String nome) {
 		this.nome = nome;
@@ -123,34 +124,38 @@ public class Aluno {
 	public void setSerieMatriculado(String serieMatriculado) {
 		this.serieMatriculado = serieMatriculado;
 	}
-	
+
 //	Método que retorna a média do aluno
 	public double getMediaNota() {
-		
+
 		double somaNotas = 0;
-		
-		for(Disciplina disciplina : disciplinas) {
+
+		for (Disciplina disciplina : disciplinas) {
 			somaNotas += disciplina.getNota();
 		}
-		
+
 		return somaNotas / disciplinas.size();
 	}
 
 //	Método retorna true para aprovado e false para reprovado
 	public boolean getAlunoAprovado() {
 		double media = this.getMediaNota();
-		if(media >= 70 ) {
+		if (media >= 50) {
 			return true;
-		}else {
+		} else {
 			return false;
 		}
 	}
-	
+
 	public String getAlunoAprovado2() {
 		double media = this.getMediaNota();
-		if(media >= 70 ) {
-			return "Aluno Aprovado";
-		}else {
+		if (media >= 50) {
+			if (media >= 70) {
+				return "Aluno Aprovado";
+			} else {
+				return "Aluno em Recuperação";
+			}
+		} else {
 			return "Aluno reprovado";
 		}
 	}
@@ -162,7 +167,9 @@ public class Aluno {
 				+ ", dataMatricula=" + dataMatricula + ", nomeEscola=" + nomeEscola + ", serieMatriculado="
 				+ serieMatriculado + ", disciplinas=" + disciplinas + "]";
 	}
-	//	Usamos o nome e o cpf neste caso para diferenciar um objeto do outro no caso os Alunos 
+
+	// Usamos o nome e o cpf neste caso para diferenciar um objeto do outro no caso
+	// os Alunos
 	@Override
 	public int hashCode() {
 		return Objects.hash(nome, numeroCpf);
@@ -179,7 +186,5 @@ public class Aluno {
 		Aluno other = (Aluno) obj;
 		return Objects.equals(nome, other.nome) && Objects.equals(numeroCpf, other.numeroCpf);
 	}
-	
-	
-	
+
 }
